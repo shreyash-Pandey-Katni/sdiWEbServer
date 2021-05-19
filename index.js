fbd30cc94ac56b767bec7847d29df29962d3a65c
shreyash <Shreyash.pandey.katni@gmail.com>
2021-05-20 00:36:55 +0530

    timetable for 6c added

--- index.js
+++ index.js
@@ -2,12 +2,31 @@ const express = require('express');
 const app = express()
 const studentRoute = require('./model/student')
 const authRoute = require('./route/auth')
+const mongoose = require('mongoose')
+const xlsx = require('xlsx');
+const path = require('path');
+
+
 const PORT = process.env.PORT || 4000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/testdb';
-const mongoose = require('mongoose')
 mongoose.connect(MONGO_URI, {useNewUrlParser:true, useUnifiedTopology:true})
 
-
+app.get('/api/timetable', (req, res) => {
+    var workbook = xlsx.readFile(path.join(__dirname, 'assets', 'timetable', 'timetable.xlsx'));
+    try {
+        if (!workbook.SheetNames.includes(`${req.headers.semester}-${req.headers.section}`)) {
+            res.sendStatus(403);
+        } else {
+        res.json(
+            xlsx.utils.sheet_to_json(workbook.Sheets[`${req.headers.semester}-${req.headers.section}`])
+        );
+        }
+        // res.sendStatus(200);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(403);
+    }
+})
 app.use('/api/students', authRoute)
 
 app.listen(PORT, () => {
--- model/student.js
+++ model/student.js
@@ -28,9 +28,71 @@ const studentSchema = new schema({
     },
     cgpa: {
         type: String,
+    },
+    semester: {
+        type: Number,
+        required: true
+    },
+    section: {
+        type: String,
+        required: true,
+    },
+    batch: {
+        type: String,
+        required: true
     }
-}, {timestamps:true})
+}, {timestamps:true});
+const professorSchema = new schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    semesters: {
+        type: [String],
+        required: true,
+    },
+});
+const questionSchema = new schema({
+    question: {
+        type: String,
+        required: true,
+    },
+    answer: {
+        type: [String],
+        required: true,
+    }
+})
+
+const mcqSchema = new schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: String,
+        required: true
+    },
+    questions: {
+        type: [questionSchema],
+        required: true,
+    },
+    startDate: {
+        type: String,
+        required: true
+    },
+    endDate: {
+        type: String,
+        required: true
+    }
+})
 
 const student = mongoose.model('Student', studentSchema);
+const professor = mongoose.model('Professor', professorSchema);
+const mcq = mongoose.model('MCQ', mcqSchema);
 
-module.exports = student;
+module.exports = {student, professor, mcq};
